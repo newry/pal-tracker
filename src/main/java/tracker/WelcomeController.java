824aca66c4fa47ccaafdd7c1c2f97b3a65838c43
pal_user <newry1979@gmail.com>
2018-02-20 15:40:12 -0800

    Simple Spring boot app

---
+++ src/main/java/tracker/WelcomeController.java
@@ -0,0 +1,13 @@
+package tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello";
+    }
+}
